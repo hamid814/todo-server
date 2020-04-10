41ba6f23bec9475150845ef4c004c46101813ea0
hamid <hamid331994@gmail.com>
2020-04-10 21:37:05 +0430

    added logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,3 +49,18 @@ exports.login = asyncHandler(async (req, res, next) => {
     token
   });
 });
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/auth/logout
+// @access    Public
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const {
   register,
-  login
+  login,
+  logout
 } = require('../controllers/auth')
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 
 module.exports = router;
